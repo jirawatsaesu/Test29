cf932dd7139a2a5a9c29c0368343876487e3d0fe
Jirawat Yuktawathin <yuk.jirawat@gmail.com>
2018-03-29 14:15:22 +0700

    Minimal html home page template

--- quiz/tests.py
+++ quiz/tests.py
@@ -3,6 +3,7 @@ from django.test import TestCase
 from quiz.views import home_page
 
 class HomePageTest(TestCase):
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
+
+    def test_uses_home_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
